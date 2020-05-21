a4dd7aa16809f1625185084fbd01be3a164dfa9d
Troger1 <reiseddy@yahoo.com>
2020-05-22 00:26:07 +0300

    [WIP] added likes api, state, epics, reducer & implemented to ui

---
+++ lib/src/epics/likes_epics.dart
@@ -0,0 +1,47 @@
+import 'package:instagram_clone/src/actions/actions.dart';
+import 'package:instagram_clone/src/actions/likes/create_like.dart';
+import 'package:instagram_clone/src/actions/likes/get_likes.dart';
+import 'package:instagram_clone/src/data/likes_api.dart';
+import 'package:instagram_clone/src/models/app_state.dart';
+import 'package:instagram_clone/src/models/likes/like.dart';
+import 'package:meta/meta.dart';
+import 'package:redux_epics/redux_epics.dart';
+import 'package:rxdart/rxdart.dart';
+
+class LikesEpics {
+  const LikesEpics({
+    @required LikesApi likesApi,
+  })  : assert(likesApi != null),
+        _api = likesApi;
+
+  final LikesApi _api;
+
+  Epic<AppState> get epics {
+    return combineEpics(<Epic<AppState>>[
+      TypedEpic<AppState, CreateLike>(_createLike),
+      TypedEpic<AppState, GetLikes>(_getLikes),
+    ]);
+  }
+
+  Stream<AppAction> _createLike(Stream<CreateLike> actions, EpicStore<AppState> store) {
+    return actions //
+        .flatMap((CreateLike action) => _api
+        .create(
+      uid: store.state.auth.user.uid,
+      parentId: action.parentId,
+      type: action.type,
+    )
+        .asStream()
+        .map<AppAction>((Like like) => CreateLikeSuccessful(like))
+        .onErrorReturnWith((dynamic error) => CreateLikeError(error)));
+  }
+
+  Stream<AppAction> _getLikes(Stream<GetLikes> actions, EpicStore<AppState> store) {
+    return actions //
+        .flatMap((GetLikes action) => _api
+        .getLikes(action.parentId)
+        .asStream()
+        .map<AppAction>((List<Like> likes) => GetLikesSuccessful(likes, action.parentId))
+        .onErrorReturnWith((dynamic error) => GetLikesError(error)));
+  }
+}
---
+++ lib/src/reducer/likes_reducer.dart
@@ -0,0 +1,52 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:instagram_clone/src/actions/likes/create_like.dart';
+import 'package:instagram_clone/src/actions/likes/get_likes.dart';
+import 'package:instagram_clone/src/models/likes/like.dart';
+import 'package:instagram_clone/src/models/likes/like_type.dart';
+import 'package:instagram_clone/src/models/likes/likes_state.dart';
+import 'package:redux/redux.dart';
+
+Reducer<LikesState> likesReducer = combineReducers<LikesState>(<Reducer<LikesState>>[
+  TypedReducer<LikesState, CreateLikeSuccessful>(_createLikeSuccessful),
+  TypedReducer<LikesState, GetLikesSuccessful>(_getLikesSuccessful),
+]);
+
+LikesState _createLikeSuccessful(LikesState state, CreateLikeSuccessful action) {
+  return state.rebuild((LikesStateBuilder b) {
+
+    if (action.like.type == LikeType.post) {
+      final ListBuilder<Like> list = b.posts[action.like.parentId]?.toBuilder() ?? ListBuilder<Like>();
+      if (!list.build().contains(action.like)) {
+        list.add(action.like);
+      }
+      b.posts[action.like.parentId] = list.build();
+    } else if (action.like.type == LikeType.comment) {
+      final ListBuilder<Like> list = b.comments[action.like.parentId]?.toBuilder() ?? ListBuilder<Like>();
+      if (!list.build().contains(action.like)) {
+        list.add(action.like);
+      }
+      b.comments[action.like.parentId] = list.build();
+    } else {
+      throw ArgumentError('Unknown like type ${action.like.type}');
+    }
+  });
+
+}
+LikesState _getLikesSuccessful(LikesState state, GetLikesSuccessful action) {
+  return state.rebuild((LikesStateBuilder b) {
+    if (action.likes.isEmpty) {
+      b.comments[action.parentId] = BuiltList<Like>();
+      b.posts[action.parentId] = BuiltList<Like>();
+    } else {
+      final LikeType type = action.likes[0].type;
+
+      if (type == LikeType.post) {
+        b.posts[action.parentId] = BuiltList<Like>(action.likes);
+      } else if (type == LikeType.comment) {
+        b.comments[action.parentId] = BuiltList<Like>(action.likes);
+      } else {
+        throw ArgumentError('Unknown like type $type');
+      }
+    }
+  });
+}
